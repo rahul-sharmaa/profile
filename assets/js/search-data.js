8257ac34abe986c81e61fa28a17a44d6d15c699d
rahul-sharmaa <rahul.gla@gmail.com>
2026-03-22 15:38:36 +0000

    Deploying to gh-pages from @ rahul-sharmaa/profile@1e0fafca69504f14875f4ffa73fcfdba372555ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "Course materials, schedules, and resources for classes taught.",
